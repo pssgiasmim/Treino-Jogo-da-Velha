8eb75da8dfc8d0cf79dc94a0a17d74309f9ed7d3
pssgiasmim <iasmimpassing@gmail.com>
2024-09-11 14:46:08 -0300

    Criação do script GameManager

---
+++ Treino Jogo da Velha/Assets/Script/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int indexI, indexJ; //Variáveis de localização para os blocos da matriz.
+
+    [SerializeField] Sprite x, circulo;
+
+    //Método para ter o endereço das áreas.
+    public void DefinirEndereco(int i, int j)
+    {
+        indexI = i;
+        indexJ = j;
+    }
+
+    //Método para ver se o botão da área foi clicado.
+    public void BotaoClicado()
+    {
+
+    }
+
+    //Método para trocar a sprite da área por  um X ou  círculo.
+    public void TrocasSprite()
+    {
+
+    }
+}
